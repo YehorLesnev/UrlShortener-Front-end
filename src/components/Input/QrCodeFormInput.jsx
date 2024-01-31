22daa95a592c76621022c99c746206aad4bf499f
Yehor Lesnevych <egor.lesnev@ukr.net>
2024-01-31 20:17:22 +0200

    Add QR code string validation

--- src/components/Input/QrCodeFormInput.jsx
+++ src/components/Input/QrCodeFormInput.jsx
@@ -1,12 +1,30 @@
 import React, { useState } from "react";
 import QrCodeImage from "../Output/QrCodeImage";
 import InputTextField from "./InputTextField";
+import { isValidInput } from "./QrCodeInputValidator";
+import { enqueueSnackbar, closeSnackbar } from "notistack";
+import { Button } from "@mui/material";
 
 const QrCodeFormInput = () => {
     const [inputValue, setInputValue] = useState('');
     const [outputValue, setOutputValue] = useState('');
 
     const handleInputChange = (event) => {
+        if (false === isValidInput(event.target.value)) {
+            enqueueSnackbar("Specified text is too long (Max length is 2331)", {
+                variant: "error",
+                autoHideDuration: 5000,
+                anchorOrigin: { vertical: "bottom", horizontal: "right" },
+                action: (key) => (
+                    <Button color="inherit" size="small" onClick={() => closeSnackbar(key)}>
+                        X
+                    </Button>
+                ),
+            });
+
+            return;
+        }
+
         setInputValue(event.target.value);
     };
 
--- src/pages/QrCodeGeneratorPage/QrCodeGeneratorPage.jsx
+++ src/pages/QrCodeGeneratorPage/QrCodeGeneratorPage.jsx
@@ -1,9 +1,7 @@
 import { SnackbarProvider } from "notistack";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
-import InputTextField from "../../components/Input/InputTextField";
 import { Box } from "@mui/system";
-import QrCodeImage from "../../components/Output/QrCodeImage";
 import QrCodeFormInput from "../../components/Input/QrCodeFormInput";
 
 function UrlShorteningPage() {
