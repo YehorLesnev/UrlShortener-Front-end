cc7f672ee6d2674069647ddb14fc02aaac99416a
Yehor Lesnevych <egor.lesnev@ukr.net>
2024-01-31 20:28:28 +0200

    Add margin to QR code image

--- src/components/Output/QrCodeImage.jsx
+++ src/components/Output/QrCodeImage.jsx
@@ -6,15 +6,17 @@ const QrCodeImage = ({
     bgColor,
     fgColor
 }) => {
+
     return (
         <QRCode
             size={256}
             viewBox={`0 0 256 256`}
-            style={{ height: "auto", width: "15%", minWidth: "128px", maxWidth: "50%" }}
+            style={{ height: "auto", width: "15%", minWidth: "128px", maxWidth: "50%", margin: "20px" }}
             value={value}
             bgColor={bgColor}
             fgColor={fgColor}
             level="M"
+            
         />
     );
 }
