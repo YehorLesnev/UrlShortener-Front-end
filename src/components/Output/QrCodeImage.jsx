12961bc3cbf50954cbcbdf60a679eadcc8cfcaec
Yehor Lesnevych <egor.lesnev@ukr.net>
2024-01-31 21:18:46 +0200

    Add download QR code button

--- src/components/Output/QrCodeImage.jsx
+++ src/components/Output/QrCodeImage.jsx
@@ -1,5 +1,22 @@
 import React from "react";
-import QRCode from "react-qr-code";
+import QRCode from "qrcode.react";
+import { IconButton, Tooltip } from "@mui/material";
+import DownloadIcon from '@mui/icons-material/Download';
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from '../../styles/Themes';
+
+const handleDownloadClick = () => {
+    const qrCodeURL = document.getElementById('qrCodeEl')
+        .toDataURL("image/png")
+        .replace("image/png", "image/octet-stream");
+        
+    let aEl = document.createElement("a");
+    aEl.href = qrCodeURL;
+    aEl.download = "QR_Code.png";
+    document.body.appendChild(aEl);
+    aEl.click();
+    document.body.removeChild(aEl);
+}
 
 const QrCodeImage = ({
     value,
@@ -8,16 +25,26 @@ const QrCodeImage = ({
 }) => {
 
     return (
-        <QRCode
-            size={256}
-            viewBox={`0 0 256 256`}
-            style={{ height: "auto", width: "15%", minWidth: "128px", maxWidth: "50%", margin: "20px" }}
-            value={value}
-            bgColor={bgColor}
-            fgColor={fgColor}
-            level="M"
-            
-        />
+        <>
+            <ThemeProvider theme={theme}>
+                <QRCode
+                    size={256}
+                    viewBox={`0 0 256 256`}
+                    style={{ height: "auto", width: "15%", minWidth: "128px", maxWidth: "50%", marginTop: "20px" }}
+                    value={value}
+                    bgColor={bgColor}
+                    fgColor={fgColor}
+                    level="M"
+                    id="qrCodeEl"
+                />
+
+                <Tooltip title="Download">
+                    <IconButton onClick={handleDownloadClick} style={{ marginTop: "1rem" }}>
+                        <DownloadIcon style={{ color: "#e5322d", height: "50px", width: "auto" }} />
+                    </IconButton>
+                </Tooltip>
+            </ThemeProvider>
+        </>
     );
 }
 
